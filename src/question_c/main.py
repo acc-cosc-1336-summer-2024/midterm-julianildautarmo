7f5529f0968d90bb35c5e365b471b9344b187aa1
julianildautarmo <149820753+julianildautarmo@users.noreply.github.com>
2024-06-25 01:30:55 +0000

    q3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,7 +1,16 @@
 #add import
 
+#part 3.3
 import question_c
 
-bonus_p = 1
+sales = 0
 
-#need to do front end stuff now
+
+sales = input("\nHow much did you sell, in US dollars, over the last sales period?\n\n")
+
+bonus = question_c.get_bonus_pay(sales)
+
+if(bonus == "\nInvalid Argument"):
+    print(bonus)
+else:
+    print("\nYou sold a total of $", sales, "of merchandise last sales period and as such are entitled to a bonus of $", bonus,"\n")
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -4,11 +4,10 @@
 
 def get_bonus_pay(sales):
     if(int(sales) < 0 or int(sales) > 1999):
-        valid = False
+        bonus = "\nInvalid Argument"
 
-        #direction wasnt given on what to do during the intervals between 499 and 500, 999 and 1000, 1499 and 1500
     else:
-        valid = True
+        #direction wasn't given on what to do during the intervals between 499 and 500, 999 and 1000, 1499 and 1500
         if(0 <= int(sales) <=499):
             percent = 0.05
         if(500 <= int(sales) <= 999):
@@ -17,11 +16,6 @@ def get_bonus_pay(sales):
             percent = 0.07
         if(1500 <= int(sales) <= 1999):
             percent = 0.08
+        bonus = float(percent) * float(sales)
 
-    if(valid == False):
-        bonus_p = "Invalid Argument"
-
-    else:
-        bonus_p = float(percent) * float(sales)
-
-    return bonus_p
+    return bonus
