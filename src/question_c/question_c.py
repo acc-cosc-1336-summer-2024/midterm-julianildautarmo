a4ab994a51c10e5050c8e17d3eaaa8727e622772
julianildautarmo <149820753+julianildautarmo@users.noreply.github.com>
2024-06-27 01:41:21 +0000

    a

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -3,18 +3,19 @@
 #Part 3.1
 
 def get_bonus_pay(sales):
-    if(int(sales) < 0 or int(sales) > 1999):
+    sales = int(sales)
+    if(sales < 0 or sales > 1999):
         bonus = "\nInvalid Argument"
 
     else:
         #direction wasn't given on what to do during the intervals between 499 and 500, 999 and 1000, 1499 and 1500
-        if(0 <= int(sales) <=499):
+        if(0 <= sales <=499):
             percent = 0.05
-        if(500 <= int(sales) <= 999):
+        if(500 <= sales <= 999):
             percent = 0.06
-        if(1000 <= int(sales) <= 1499):
+        if(1000 <= sales <= 1499):
             percent = 0.07
-        if(1500 <= int(sales) <= 1999):
+        if(1500 <= sales <= 1999):
             percent = 0.08
         bonus = float(percent) * float(sales)
 
